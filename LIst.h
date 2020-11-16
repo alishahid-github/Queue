f7a938ef20dc3948e488bac7e6494478a115c744
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-11-16 08:49:06 +0500

    Linked list for Queue

---
+++ LIst.h
@@ -0,0 +1,23 @@
+//Header file for the Linked List with Basic funtions
+
+#include<iostream>
+#include"Node.h"
+#ifndef LIST
+#define LIST
+using namespace std;
+class List {
+public:
+	List();
+	void insertH(int d);		// d=data
+	int delete_listT();
+	void print();
+	void set_head(Node* h);
+	Node* get_head();		// usage will be in main on neeed base
+	int node_count = 0;
+
+private:
+	Node* head;
+
+};
+
+#endif
