253d5c6f7d3bb11ac3931ecc139db00d89a13179
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-11-16 08:50:10 +0500

    Update LIst.h

--- LIst.h
+++ LIst.h
@@ -1,4 +1,4 @@
-//Header file for the Linked List with Basic funtions
+//Header file for the Linked List with Basic funtions for Queue
 
 #include<iostream>
 #include"Node.h"
@@ -8,11 +8,11 @@ using namespace std;
 class List {
 public:
 	List();
-	void insertH(int d);		// d=data
+	void insertH(int d);		// d=data of the node
 	int delete_listT();
 	void print();
 	void set_head(Node* h);
-	Node* get_head();		// usage will be in main on neeed base
+	Node* get_head();		
 	int node_count = 0;
 
 private:
