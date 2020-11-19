7cc49744012969bdd248e9d3fba589b59c5c8123
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-11-19 08:04:32 +0500

    Queue header file

---
+++ Queue.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"List.h"
+#ifndef QUEUE
+#define QUEUE
+using namespace std;
+class Queue
+{
+private:
+	List L1;
+public:
+	void Enqueue(int d);
+	int Dequeue();
+	void print();
+};
+
+
+#endif
