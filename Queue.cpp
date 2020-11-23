271b8dee949dabb42259362b6520400b892e288e
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-11-23 15:06:17 +0500

    Implementation of Queue file

---
+++ Queue.cpp
@@ -0,0 +1,27 @@
+#include "Queue.h"
+#include "Node.h"
+
+void Queue::Enqueue(int d)
+{
+	L1.insertH(d);
+}
+
+int Queue::Dequeue()
+{
+	return L1.delete_listT();
+}
+
+void Queue::print()
+{
+	if (L1.get_head() == NULL)
+		cout << "\nQueue is empty";
+	else
+	{
+		Node* temp = L1.get_head();
+		while(temp != NULL)
+		{ 
+			cout << "\tvalue is = "<<temp->get_data();
+			temp = temp->get_next();
+		}
+	}
+}
